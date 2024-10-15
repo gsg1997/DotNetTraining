76a86be6e5dbd03cfafe97e40e802326d76211d2
gsg1997 <124980874+gsg1997@users.noreply.github.com>
2024-10-15 12:10:25 +0530

    Add files via upload

---
+++ Milestone5/Milestone5/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DatabaseDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Server=using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DatabaseDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Server=WIPDNFSD;Dtabase=SampleDB;User Id=GSG1997;Password=SEC@123;";
+            List<User> users = new List<User>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string query = "SELECT * FROM Users";
+                    SqlCommand command = new SqlCommand(query, connection);
+
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        User user = new User
+                        {
+                            Id = reader.GetInt32(0),
+                            Name = reader.GetString(1),
+                            Email = reader.GetString(2)
+                        };
+                        users.Add(user);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("An error occurred: " + ex.Message);
+                }
+            }
+
+            // Displaying the users
+            foreach (var user in users)
+            {
+                Console.WriteLine($"ID: {user.Id}, Name: {user.Name}, Email: {user.Email}");
+            }
+        }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
+;Database=SampleDB;User Id=your_username;Password=your_password;";
+            List<User> users = new List<User>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string query = "SELECT * FROM Users";
+                    SqlCommand command = new SqlCommand(query, connection);
+
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        User user = new User
+                        {
+                            Id = reader.GetInt32(0),
+                            Name = reader.GetString(1),
+                            Email = reader.GetString(2)
+                        };
+                        users.Add(user);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("An error occurred: " + ex.Message);
+                }
+            }
+
+            // Displaying the users
+            foreach (var user in users)
+            {
+                Console.WriteLine($"ID: {user.Id}, Name: {user.Name}, Email: {user.Email}");
+            }
+        }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Milestone5/Milestone5/Selenium.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://Amazon.com/login");
+
+            // Locate username and password fields and login button
+            driver.FindElement(By.Id("username")).SendKeys("testuser");
+            driver.FindElement(By.Id("password")).SendKeys("password123");
+            driver.FindElement(By.XPath("//button[text()='Login']")).Click();
+
+            // Assertion to verify login success
+            string currentUrl = driver.Url;
+            if (currentUrl.Contains("dashboard"))
+            {
+                Console.WriteLine("Login successful. Current URL: " + currentUrl);
+            }
+            else
+            {
+                Console.WriteLine("Login failed.");
+            }
+
+            driver.Quit();
+        }
+    }
+}
